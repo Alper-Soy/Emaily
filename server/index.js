cb3a19b84e3712ee425b90a89a9705a8352ae49f
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2021-01-08 16:38:17 +0300

    Adding authentication with google OAUTH

--- server/index.js
+++ server/index.js
@@ -1,27 +1,14 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+const morgan = require('morgan');
+require('./services/passport');
+
+// Routes
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
-    },
-    (accessToken, refreshToken, profile, cb) => {
-      console.log(accessToken);
-    }
-  )
-);
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] })
-);
+app.use(morgan('dev'));
+app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
---
+++ server/routes/authRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const passport = require('passport');
+
+const route = express.Router();
+
+route.get(
+  '/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+// after client grants permission
+// with code that google sent
+// passportjs automaticly makes for us this situation.
+route.get('/google/callback', passport.authenticate('google'));
+
+module.exports = route;
