4d0c08bd56e2f5cd5e708ab3372a38cae89c9ce8
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2021-01-14 16:06:22 +0300

    Verifying Minimum Credits

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -2,11 +2,10 @@ const express = require('express');
 const Survey = require('../models/survey');
 
 const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
 
 const route = express.Router();
 
-route.post('/surveys', requireLogin, (req, res) => {
-    
-});
+route.post('/surveys', requireLogin, requireCredits, (req, res) => {});
 
 module.exports = route;
