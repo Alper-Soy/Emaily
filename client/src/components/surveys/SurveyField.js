c7bdfd2538ad09307b94fdfe62579a4e77101396
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2021-01-16 23:38:42 +0300

    Adding form survey review component

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,15 +1,16 @@
+// SurveyField contains logic to render a single
+// label and text input
 import React from 'react';
 
-const SurveyField = ({ input, label, meta: { error, touched } }) => {
+// eslint-disable-next-line import/no-anonymous-default-export
+export default ({ input, label, meta: { error, touched } }) => {
   return (
     <div>
       <label>{label}</label>
-      <input style={{ marginBottom: '5px' }} {...input} />
+      <input {...input} style={{ marginBottom: '5px' }} />
       <div className='red-text' style={{ marginBottom: '20px' }}>
         {touched && error}
       </div>
     </div>
   );
 };
-
-export default SurveyField;
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,20 +1,14 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
-import SurveyField from './SurveyField';
-import _ from 'lodash';
 import { Link } from 'react-router-dom';
+import SurveyField from './SurveyField';
 import validEmails from '../../utils/validateEmails';
-
-const FIELDS = [
-  { label: 'Survey Title', name: 'title' },
-  { label: 'Subject Line', name: 'subject' },
-  { label: 'Email Body', name: 'body' },
-  { label: 'Recipient List', name: 'emails' },
-];
+import formFields from './formFields';
 
 class SurveyForm extends Component {
   renderFields() {
-    return _.map(FIELDS, ({ label, name }) => {
+    return _.map(formFields, ({ label, name }) => {
       return (
         <Field
           key={name}
@@ -30,9 +24,7 @@ class SurveyForm extends Component {
   render() {
     return (
       <div>
-        <form
-          onSubmit={this.props.handleSubmit((values) => console.log(values))}
-        >
+        <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
           {this.renderFields()}
           <Link to='/surveys' className='red btn-flat white-text'>
             Cancel
@@ -50,18 +42,17 @@ class SurveyForm extends Component {
 function validate(values) {
   const errors = {};
 
-  errors.emails = validEmails(values.emails || '');
-
-  _.each(FIELDS, ({ name }) => {
+  _.each(formFields, ({ name }) => {
     if (!values[name]) {
       errors[name] = 'You must provide a value';
     }
   });
 
+  errors.emails = validEmails(values.emails || '');
   return errors;
 }
-
 export default reduxForm({
   validate,
   form: 'surveyForm',
+  destroyOnUnmount: false,
 })(SurveyForm);
